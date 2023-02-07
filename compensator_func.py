0df889c3ae90c2219f75a42a7932b28d02e651a8
dsyang1024 <31736813+dsyang1024@users.noreply.github.com>
2023-02-07 12:41:16 -0500

    function script made

---
+++ compensator_func.py
@@ -0,0 +1,14 @@
+
+def make_graph(title, x1title, x1list, x2title, x2list):
+'''
+this script is making graph for the processed data from main.py
+users should make sure that list variable should contain 'only' float format numbers not string
+if you are using this function as part of compensator, you don't need to worry about
+title : title for the graph, should be string
+x1 : variable x1 (left side)
+x2 : variable x2 (right side)
+title : title of each axis
+list : list of the variable
+'''
+
+import seaborn
