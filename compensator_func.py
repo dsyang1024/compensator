e6c800feced17918cbf44a8022868cd2a701eb1c
dsyang1024 <31736813+dsyang1024@users.noreply.github.com>
2023-02-09 17:31:02 -0500

    history function updated

--- compensator_func.py
+++ compensator_func.py
@@ -4,12 +4,13 @@ This library is including multiple functions for compensation
 2. comp : compensation function for the loggers using baro
 3. baro_crawler : crawling baro data from the purdue airport station
 4. baro_cali : calibrate baro values(pressure) of the field, using one from purdue airport station
+5. history
 '''
 
 
 def graphy(title, xtitle, xlist, y1title, y1list, y2title, y2list):
     '''
-    this script is making graph for the processed data from main.py
+    this function is making graph for the processed data from main.py
     users should make sure that list variable should contain 'only' float format numbers not string
     if you are using this function as part of compensator, you don't need to worry about
     title : title for the graph, should be string
@@ -53,11 +54,82 @@ def graphy(title, xtitle, xlist, y1title, y1list, y2title, y2list):
 
 
 def comp():
-    # Need to read setup file for compensation.
-    # this compensation setting file inclues depth of the logger in the field
+    '''
+    need to read setup file for compensation.
+    this compensation setting file inclues depth of the logger in the field
+    ''' 
     print('this is function [comp]')
 
 def history():
+    '''
+    this function make history log file with file used for the compensation
+    if the file read, it will be recorded to the history.log file in text format
+    all the data (.csv) should be contained in the /data/ directory
+    the data will be collected depends on the monitoring station, this function OUT, ILA, ILB, CEN
+    '''
+    import os
+    data_list = os.listdir('data/')
+    # print(data_list)
+    OUTfile = [i for i in data_list if 'OUT' in i]
+    # print(OUTfile)
+    ILAfile = [i for i in data_list if 'ILA' in i]
+    # print(ILAfile)
+    ILBfile = [i for i in data_list if 'ILB' in i]
+    # print(ILBfile)
+    CENfile = [i for i in data_list if 'CEN' in i]
+    # print(CENfile)
+
+
+    # Read history file, if it does not exist, make a new one
+    try:
+        with open('history.log','r') as fhistory:
+            olddata_list = fhistory.readlines()
+        print('Reading history log...')
+        # index [:-1] will make file name in the list without '\n'
+        oldOUTfile = [i[:-1] for i in olddata_list if 'OUT' in i]
+        oldILAfile = [i[:-1] for i in olddata_list if 'ILA' in i]
+        oldILBfile = [i[:-1] for i in olddata_list if 'ILB' in i]
+        oldCENfile = [i[:-1] for i in olddata_list if 'CEN' in i]
+        print('\n\n::: Previous Data status :::','\nOUT :: ',len(oldOUTfile),'\nILA :: ',len(oldILAfile),'\nILB :: ',len(oldILBfile),'\nCEN :: ',len(oldCENfile))
+    except:
+        print('[Error 02] File doesnot exist and made a new file')
+        fhistory = open('history.log','w')
+        fhistory.close()
+        # these empty list will prevent error when merging old/new lists below
+        oldOUTfile = []
+        oldILAfile = []
+        oldILBfile = []
+        oldCENfile = []
+
+
+    # Check there is new data or not for each station
+    # is OUTfile item is in the oldOUTfile list, it will be removed
+    OUTfile = [i for i in OUTfile if i not in oldOUTfile]    
+    ILAfile = [i for i in ILAfile if i not in oldILAfile]
+    ILBfile = [i for i in ILBfile if i not in oldILBfile]
+    CENfile = [i for i in CENfile if i not in oldCENfile]
+    print('\n\n::: Updated Data status :::','\nOUT :: ',len(OUTfile),'\nILA :: ',len(ILAfile),'\nILB :: ',len(ILBfile),'\nCEN :: ',len(CENfile))
+
+
+
+    # merge old list and new list
+    newOUTfile = oldOUTfile + OUTfile
+    newOUTfile.sort()
+    newILAfile = oldILAfile + ILAfile
+    newILAfile.sort()
+    newILBfile = oldILBfile + ILBfile
+    newILBfile.sort()
+    newCENfile = oldCENfile + CENfile
+    newCENfile.sort()
+
+
+    with open('history.log','w') as fhistory:
+        for i in [newOUTfile, newILAfile, newILBfile, newCENfile]:
+            for r in i:
+                fhistory.write(r)
+                fhistory.write('\n')
+            fhistory.write('\n\n')
+    print('history log file updated')
 
 
 
@@ -70,4 +142,5 @@ y1list = np.random.rand(200)
 y2title = 'Baro pressure (kPa)'
 y2list = np.random.rand(200)
 
-graphy(title, xtitle, xlist, y1title, y1list, y2title, y2list)
+# graphy(title, xtitle, xlist, y1title, y1list, y2title, y2list)
+history()
